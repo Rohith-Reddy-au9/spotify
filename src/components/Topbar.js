a12190edb6ea1909a7f9edb641a9d39beb69961c
Rohith Reddy <pulagamrr@gmail.com>
2021-01-21 11:50:00 +0530

    spotify

---
+++ src/components/Topbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Topbar.css'
+const Topbar = () => {
+    return(
+        <div className = "Topbar">
+            Topbar
+        </div>
+    )
+}
+
+export default Topbar
